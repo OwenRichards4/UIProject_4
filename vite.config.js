a251c6a094338ab96abaa11f9735fe769242ca24
Owen Richards <owencrichards@gmail.com>
2024-12-08 15:48:10 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/User_Interface_Project_4/",
+  base: "/UIProject_4/",
   plugins: [svelte()],
 })
